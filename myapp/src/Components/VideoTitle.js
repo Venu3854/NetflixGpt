98f345c7ffd7f3ebc809d0538a001578226af0a1
Venukumar Shivagari <vshiva3854@gmail.com>
2024-01-09 16:16:01 +0530

    heading text change

--- myapp/src/Components/VideoTitle.js
+++ myapp/src/Components/VideoTitle.js
@@ -3,7 +3,7 @@ import React from "react";
 const VideoTitle = ({ title, overview }) => {
   return (
     <div className="w-screen aspect-video pt-[15%] px-24 absolute text-white bg-gradient-to-r from-black">
-      <h1 className="text-6xl font-bold">{title}</h1>
+      <h1 className="text-5xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/4">{overview}</p>
       <div>
         <button className="bg-white hover:bg-opacity-80 text-white p-3 px-10 text-lg bg-opacity-50 rounded-lg">
